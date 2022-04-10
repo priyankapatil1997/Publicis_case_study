afc3b50599b93eea85c25223e8ab9cb5916830ad
priyanka <789patilpriyanka@gmail.com>
2022-04-10 19:34:25 +0530

    final-commit

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux"
 import { allCategoryData, getAllproduct } from "../../redux/action";
+import './home.css'
 
 const Home = () => {
   const dispatch = useDispatch()
