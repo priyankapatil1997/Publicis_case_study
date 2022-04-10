38b26523fff3c9b26fbc765a2928d26441c7b0e8
priyanka <789patilpriyanka@gmail.com>
2022-04-10 19:23:48 +0530

    in-orogress

--- src/component/pagenotfound/PageNotFound.js
+++ src/component/pagenotfound/PageNotFound.js
@@ -17,7 +17,7 @@ const PageNotFound = () => {
                                     Look like you're lost
                                 </h3>
 
-                                <p>the page you are looking for not avaible!</p>
+                                <p>the page you are looking for not available</p>
 
                                 <Link to="/" class="link_404">Go to Home</Link>
                             </div>
